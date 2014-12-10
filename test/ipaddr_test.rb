5628296696cabd491cecfc3587b91d7eb14c04fe
Bryan T. Richardson <bryan@activeshadow.com>
2014-12-10 12:32:06 -0700

    Modified IPAddr tests to account for reduced capability in IPAddr monkey patch

--- test/ipaddr_test.rb
+++ test/ipaddr_test.rb
@@ -1,32 +1,22 @@
 require 'test_helper'
 
 class IPAddrTest < TestCase
-  test 'tracks provided netmask' do
-    assert_equal IPAddr.new('255.0.0.0'),       IPAddr.new('192.168.1.1/8').netmask
-    assert_equal IPAddr.new('255.255.0.0'),     IPAddr.new('192.168.1.1/16').netmask
-    assert_equal IPAddr.new('255.255.255.0'),   IPAddr.new('192.168.1.1/24').netmask
-    assert_equal IPAddr.new('255.255.255.224'), IPAddr.new('192.168.1.1/27').netmask
-  end
-
-  test 'correctly determines CIDR length' do
+  test 'correctly determines prefix length' do
     assert_equal 8,  IPAddr.new('192.168.1.1/8').prefix
     assert_equal 16, IPAddr.new('192.168.1.1/16').prefix
     assert_equal 24, IPAddr.new('192.168.1.1/24').prefix
     assert_equal 27, IPAddr.new('192.168.1.1/27').prefix
   end
 
-  test 'correctly provides CIDR string' do
-    assert_equal '192.168.1.1/8',   IPAddr.new('192.168.1.1/8').to_cidr_string
-    assert_equal '192.168.1.1/16',  IPAddr.new('192.168.1.1/16').to_cidr_string
-    assert_equal '192.168.1.1/24',  IPAddr.new('192.168.1.1/24').to_cidr_string
-    assert_equal '192.168.1.33/27', IPAddr.new('192.168.1.33/27').to_cidr_string
+  test 'correctly modifies prefix length' do
   end
 
-  test 'correctly determines broadcast address' do
-    assert_equal IPAddr.new('192.255.255.255'), IPAddr.new('192.168.1.1/8').broadcast
-    assert_equal IPAddr.new('192.168.255.255'), IPAddr.new('192.168.1.1/16').broadcast
-    assert_equal IPAddr.new('192.168.1.255'),   IPAddr.new('192.168.1.1/24').broadcast
-    assert_equal IPAddr.new('192.168.1.31'),    IPAddr.new('192.168.1.1/27').broadcast
+  test 'correctly provides CIDR string' do
+    assert_equal '192.168.1.1/32',  IPAddr.new('192.168.1.1').to_cidr_string
+    assert_equal '192.0.0.0/8',     IPAddr.new('192.168.1.1/8').to_cidr_string
+    assert_equal '192.168.0.0/16',  IPAddr.new('192.168.1.1/16').to_cidr_string
+    assert_equal '192.168.1.0/24',  IPAddr.new('192.168.1.1/24').to_cidr_string
+    assert_equal '192.168.1.32/27', IPAddr.new('192.168.1.33/27').to_cidr_string
   end
 
   test 'correctly detects loopback address' do
@@ -43,22 +33,5 @@ class IPAddrTest < TestCase
     assert  IPAddr.new('10.0.0.1').private?
     assert  IPAddr.new('10.255.255.254').private?
     assert !IPAddr.new('172.32.1.1').private?
-  end
-
-  test 'correctly detects multicast address' do
-    assert  IPAddr.new('224.0.0.1').multicast?
-    assert  IPAddr.new('224.239.255.254').multicast?
-    assert !IPAddr.new('240.0.0.1').multicast?
-  end
-
-  test 'correctly detects if an address is in a network' do
-    assert  IPAddr.new('192.168.1.0/27').include?(IPAddr.new('192.168.1.1'))
-    assert  IPAddr.new('192.168.1.0/27').include?(IPAddr.new('192.168.1.30'))
-    assert !IPAddr.new('192.168.1.0/27').include?(IPAddr.new('192.168.1.33'))
-  end
-
-  test 'correctly detects if a network is in another network' do
-    assert  IPAddr.new('192.168.1.0/24').include?(IPAddr.new('192.168.1.0/27'))
-    assert !IPAddr.new('192.168.1.0/27').include?(IPAddr.new('192.168.1.0/24'))
   end
 end
